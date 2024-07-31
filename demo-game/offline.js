0ff36282e6db896903597e17b8effca06b9ab7d4
Nawamin Wongpakham <96173560+Nawamin5307@users.noreply.github.com>
2024-07-31 23:00:25 +0700

    demo

---
+++ demo-game/offline.js
@@ -0,0 +1,24 @@
+﻿{
+	"version": 1722441562,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/groud-sheet0.png",
+		"images/player-sheet0.png",
+		"images/potion-sheet0.png",
+		"images/tiledbackground.png",
+		"images/sprite-sheet0.png",
+		"images/tiledbackground2.png",
+		"media/cat-meow-loud-225307.ogg",
+		"media/pop-39222.ogg",
+		"media/high-energy-chiptune-for-platform-games-225292.ogg",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
